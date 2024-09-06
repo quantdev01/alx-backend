5064ef99d53647d140b69c65dd1f29623b07dcf9
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-09-06 21:57:00 +0200

    Task 0 done

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Return the range index
+"""
+
+
+def index_range(page, page_size):
+    """
+    Function to return range to print based on page we are on 
+    and the page size
+    """
+    last_index = page_size * page
+    first_index = last_index - page_size
+
+    return (first_index, last_index)
+

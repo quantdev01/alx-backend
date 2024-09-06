be714b54d5ca4466215dee3c207dd954933b6d13
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-09-07 00:04:43 +0200

    Task 2 Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+"""
+1. Simple pagination
+"""
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        # assert page < 0
+        # assert page_size < 0
+        page_index = index_range(page, page_size)
+
+        mydataset = self.dataset()
+
+        data = []
+
+        try:
+            # for i in range(page_index[0], page_index[1]):
+            # data = mydataset[i]
+            data = mydataset[page_index[0]:page_index[1]]
+        except IndexError:
+            return []
+
+        return data

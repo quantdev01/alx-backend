f496b3260fc70da0cc9a2adcb413ecce33759699
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-09-06 21:58:11 +0200

    Fixing pycodestyle issues

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,11 +7,10 @@ Return the range index
 
 def index_range(page, page_size):
     """
-    Function to return range to print based on page we are on 
+    Function to return range to print based on page we are on
     and the page size
     """
     last_index = page_size * page
     first_index = last_index - page_size
 
     return (first_index, last_index)
-
